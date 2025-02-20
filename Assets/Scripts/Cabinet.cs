156b5b6d35cc1a138b6653cfccd45966ef1e349f
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-02-21 03:19:07 +0330

    Drag & Drop system

---
+++ Assets/Scripts/Cabinet.cs
@@ -0,0 +1,27 @@
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cabinet : MonoBehaviour
+{
+    private RectTransform cabinetSize;
+    private GridLayoutGroup placeHolder;
+
+    private void Awake()
+    {
+        cabinetSize= gameObject.GetComponent<RectTransform>();
+        placeHolder = gameObject.GetComponent<GridLayoutGroup>();
+        placeHolder.cellSize = new Vector2(cabinetSize.rect.width / 3,cabinetSize.rect.height);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
