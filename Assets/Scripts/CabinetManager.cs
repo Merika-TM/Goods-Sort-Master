07e07ffbbef83aceab095111f1dea60eded27f2c
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-02-27 11:18:23 +0330

    Random Item Spawning

--- Assets/Scripts/CabinetManager.cs
+++ Assets/Scripts/CabinetManager.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class CabinetManager : MonoBehaviour
 {
-    public List<DraggableItem> items; 
-    public List<Cabinet> cabnets; 
-    public List<CabinetSlot> cabinetSlots; 
-    
+    public List<DraggableItem> items;
+    public List<Cabinet> cabnets;
+    public List<CabinetSlot> cabinetSlots;
+
     void Start()
     {
         for (int i = 0; i < cabnets.Count; i++)
         {
             cabinetSlots.AddRange(cabnets[i].GetComponent<Cabinet>().GetSlots());
         }
-        
+
         int slotIndex = 0;
+        int newSlotIndex;
+        
         foreach (var item in items)
         {
-            InstantiateItem(item.gameObject,cabinetSlots[slotIndex].transform);
-            slotIndex = (slotIndex + 1) % cabinetSlots.Count;
+            for (int i = 0; i < 3; i++)
+            {
+                do
+                {
+                    newSlotIndex = Random.Range(0, cabinetSlots.Count);
+                } while (newSlotIndex == slotIndex);
+                slotIndex = newSlotIndex;
+                
+                InstantiateItem(item.gameObject, cabinetSlots[slotIndex].transform);
+            }
         }
     }
+
     public void InstantiateItem(GameObject item, Transform slotTransform)
     {
         GameObject S = Instantiate(item, slotTransform);
         S.transform.localPosition = Vector3.zero;
     }
-    
 }
