6939b83fce70dd78e454621b86ad27683ebc3143
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-02-28 20:26:15 +0330

    New CheckName system

--- Assets/Scripts/CabinetSlot.cs
+++ Assets/Scripts/CabinetSlot.cs
@@ -3,35 +3,36 @@ using UnityEngine;
 
 public class CabinetSlot : MonoBehaviour, IDropHandler
 {
-
     public void OnDrop(PointerEventData eventData)
     {
         GameObject droppedItem = eventData.pointerDrag;
-        DraggableItem draggableItem = droppedItem.GetComponent<DraggableItem>();
+        if (droppedItem == null) return;
 
-        if (transform.childCount == 0)
+        DraggableItem draggableItem = droppedItem.GetComponent<DraggableItem>();
+        if (draggableItem == null) return;
+
+        if (CanAcceptItem(draggableItem))
         {
-            draggableItem.parentAtferDrag = transform;
             draggableItem.transform.SetParent(transform);
-            draggableItem.transform.localPosition = Vector3.zero;
+            draggableItem.transform.localPosition = new Vector3(0,-35,0);
+            draggableItem.parentAtferDrag = transform;
+            gameObject.GetComponentInParent<Cabinet>().CheckedLastItem();
         }
-        else 
+    }
+
+    public bool CanAcceptItem(DraggableItem newItem)
+    {
+        int lastChildIndex = transform.childCount - 1;
+        if (lastChildIndex >= 0)
         {
-            for (int i = 0; i < transform.childCount; i++)
+            Transform lastChild = transform.GetChild(lastChildIndex);
+            DraggableItem lastDraggableItem = lastChild.GetComponent<DraggableItem>();
+            if (lastDraggableItem != null && lastDraggableItem.GetActivate())
             {
-                if (transform.GetChild(i).GetComponent<DraggableItem>().GetActivate())
-                {
-                    draggableItem.transform.SetParent(draggableItem.parentAtferDrag);
-                    draggableItem.transform.localPosition = Vector3.zero;                    
-                }
-                else
-                {
-                    draggableItem.parentAtferDrag = transform;
-                    draggableItem.transform.SetParent(transform);
-                    draggableItem.transform.localPosition = Vector3.zero;
-                }
-   
+                return false;
             }
         }
+
+        return true;
     }
 }
